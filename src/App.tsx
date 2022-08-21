52b4861f6bb47db5e1593ec0698cc59aa69ba9bd
acgotaku <acgotaku311@gmail.com>
2022-08-21 23:06:19 +0900

    feat: styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
+import '@/styles/index.css';
 
 function App() {
   const [count, setCount] = useState(0);
