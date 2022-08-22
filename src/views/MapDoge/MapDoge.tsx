c2f084fa57c0732112d9b848a36bc394c7af9e4e
acgotaku <acgotaku311@gmail.com>
2022-08-22 23:27:15 +0900

    feat: query gps

--- src/views/MapDoge/MapDoge.tsx
+++ src/views/MapDoge/MapDoge.tsx
@@ -1,8 +1,10 @@
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Button, Input } from '@/components';
 import styles from './mapdoge.module.css';
 
 const MapDoge = () => {
+  const [lat, setLat] = useState('');
+  const [lng, setLng] = useState('');
   const getCurrentTab = useCallback(async () => {
     const queryOptions = { active: true, lastFocusedWindow: true };
     const [tab] = await chrome.tabs.query(queryOptions);
@@ -11,14 +13,34 @@ const MapDoge = () => {
 
   useEffect(() => {
     getCurrentTab().then(tab => {
-      console.log(tab);
+      if (tab && tab.url?.startsWith('https://www.google.com/maps')) {
+        const regex = /@([0-9.]+),([0-9.]+),([0-9.]+)z/g;
+        const result = regex.exec(tab.url);
+        if (result) {
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
+          const [_, lat, lng] = result;
+          setLat(lat);
+          setLng(lng);
+        }
+      }
     });
   }, [getCurrentTab]);
   return (
     <div className={styles.doge}>
       Map doge
-      <Button>hello button</Button>
-      <Input />
+      <form>
+        <div>
+          <label>lat</label>
+          <Input name="lat" value={lat} />
+        </div>
+        <div>
+          <label>lng</label>
+          <Input name="lng" value={lng} />
+        </div>
+        <div>
+          <Button>submit </Button>
+        </div>
+      </form>
     </div>
   );
 };
